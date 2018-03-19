fixed gradients bug with sqrt

evaluate.py
@@ -16,9 +16,9 @@ from model.utils import set_logger
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_dir', default='experiments/test',
                     help="Experiment directory containing params.json")
-parser.add_argument('--data_dir', default='data/64x64_SIGNS',
+parser.add_argument('--data_dir', default='data/mnist',
                     help="Directory containing the dataset")
-parser.add_argument('--restore_from', default='best_weights',
+parser.add_argument('--restore_from', default='last_weights',
                     help="Subdirectory of model dir or file containing the weights")
 
 
@@ -36,21 +36,14 @@ if __name__ == '__main__':
     set_logger(os.path.join(args.model_dir, 'evaluate.log'))
 
     # Create the input data pipeline
-    logging.info("Creating the dataset...")
-    data_dir = args.data_dir
-    test_data_dir = os.path.join(data_dir, "test_signs")
+    logging.info("Creating the datasets...")
+    data = tf.contrib.learn.datasets.mnist.load_mnist(args.data_dir)
 
-    # Get the filenames from the test set
-    test_filenames = os.listdir(test_data_dir)
-    test_filenames = [os.path.join(test_data_dir, f) for f in test_filenames if f.endswith('.jpg')]
-
-    test_labels = [int(f.split('/')[-1][0]) for f in test_filenames]
-
-    # specify the size of the evaluation set
-    params.eval_size = len(test_filenames)
+    # Specify the sizes of the dataset we train on and evaluate on
+    params.eval_size = data.test.num_examples
 
     # create the iterator over the dataset
-    test_inputs = input_fn(False, test_filenames, test_labels, params)
+    test_inputs = input_fn(False, data.test.images, data.test.labels, params)
 
     # Define the model
     logging.info("Creating the model...")

model/model_fn.py
@@ -4,10 +4,11 @@ import numpy as np
 import tensorflow as tf
 
 from model.triplet_loss import batch_all_triplet_loss
+from model.triplet_loss import batch_hard_triplet_loss
 
 
 def build_model(is_training, images, params):
-    """Compute logits of the model (output distribution)
+    """Compute outputs of the model (embeddings for triplet loss).
 
     Args:
         is_training: (bool) whether we are training or not
@@ -32,7 +33,7 @@ def build_model(is_training, images, params):
             out = tf.nn.relu(out)
             out = tf.layers.max_pooling2d(out, 2, 2)
 
-    assert out.get_shape().as_list() == [None, 7, 7, num_channels * 2]
+    assert out.shape[1:] == [7, 7, num_channels * 2]
 
     out = tf.reshape(out, [-1, 7 * 7 * num_channels * 2])
     with tf.variable_scope('fc_1'):
@@ -57,7 +58,7 @@ def model_fn(mode, inputs, params, reuse=False):
     is_training = (mode == 'train')
 
     images = inputs['images']
-    assert images.get_shape().as_list() == [None, params.image_size, params.image_size, 1], images
+    assert images.shape[1:] == [params.image_size, params.image_size, 1]
 
     labels = inputs['labels']
     labels = tf.cast(labels, tf.int64)
@@ -71,7 +72,15 @@ def model_fn(mode, inputs, params, reuse=False):
 
     # Define loss and accuracy
     #loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-    loss, fraction = batch_all_triplet_loss(labels, embeddings, margin=params.margin)
+    if params.triplet_strategy == "batch_all":
+        loss, fraction = batch_all_triplet_loss(labels, embeddings, margin=params.margin,
+                                                squared=params.squared)
+    elif params.triplet_strategy == "batch_hard":
+        loss = batch_hard_triplet_loss(labels, embeddings, margin=params.margin,
+                                      squared=params.squared)
+    else:
+        raise ValueError("Triplet strategy not recognized: {}".format(params.triplet_strategy))
+
     #accuracy = tf.reduce_mean(tf.cast(tf.equal(labels, predictions), tf.float32))
 
     # Define training step that minimizes the loss with the Adam optimizer
@@ -104,7 +113,8 @@ def model_fn(mode, inputs, params, reuse=False):
 
     # Summaries for training
     tf.summary.scalar('loss', loss)
-    tf.summary.scalar('fraction_positive_triplets', fraction)
+    if params.triplet_strategy == "batch_all":
+        tf.summary.scalar('fraction_positive_triplets', fraction)
     #tf.summary.scalar('accuracy', accuracy)
     tf.summary.image('train_image', images)
 

model/tests/test_triplet_loss.py
@@ -36,177 +36,193 @@ def pairwise_distance_np(feature, squared=False):
     return pairwise_distances
 
 
-class TripletLossTest(unittest.TestCase):
-    """Basic test cases."""
+def test_pairwise_distances():
+    num_data = 64
+    feat_dim = 6
 
-    def test_pairwise_distances(self):
-        num_data = 64
-        feat_dim = 6
+    embeddings = np.random.randn(num_data, feat_dim).astype(np.float32)
+    embeddings[1] = embeddings[0]  # to get distance 0
 
-        embeddings = np.random.randn(num_data, feat_dim)
+    with tf.Session() as sess:
+        for squared in [True, False]:
+            res_np = pairwise_distance_np(embeddings, squared=squared)
+            res_tf = sess.run(_pairwise_distances(embeddings, squared=squared))
+            assert np.allclose(res_np, res_tf)
 
+
+def test_gradients_pairwise_distances():
+    num_data = 64
+    feat_dim = 6
+
+    embeddings = np.random.randn(num_data, feat_dim).astype(np.float32)
+    embeddings[1] = embeddings[0]  # to get distance 0
+    embeddings = tf.constant(embeddings)
+
+    with tf.Session() as sess:
+        for squared in [True, False]:
+            dists = _pairwise_distances(embeddings, squared=squared)
+            grads = tf.gradients(dists, embeddings)
+
+            g = sess.run(grads)
+            assert not np.any(np.isnan(g)), "Gradient shouldn't be nan, squared={}".format(squared)
+
+
+def test_triplet_mask():
+    num_data = 64
+    num_classes = 10
+
+    labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
+
+    mask_np = np.zeros((num_data, num_data, num_data))
+    for i in range(num_data):
+        for j in range(num_data):
+            for k in range(num_data):
+                distinct = (i != j and i != j and j != k)
+                valid = (labels[i] == labels[j]) and (labels[i] != labels[k])
+                mask_np[i, j, k] = (distinct and valid)
+
+    mask_tf = _get_triplet_mask(labels)
+    with tf.Session() as sess:
+        mask_tf_val = sess.run(mask_tf)
+
+    assert np.allclose(mask_np, mask_tf_val)
+
+
+def test_anchor_positive_triplet_mask():
+    num_data = 64
+    num_classes = 10
+
+    labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
+
+    mask_np = np.zeros((num_data, num_data))
+    for i in range(num_data):
+        for j in range(num_data):
+            distinct = (i != j)
+            valid = labels[i] == labels[j]
+            mask_np[i, j] = (distinct and valid)
+
+    mask_tf = _get_anchor_positive_triplet_mask(labels)
+    with tf.Session() as sess:
+        mask_tf_val = sess.run(mask_tf)
+
+    assert np.allclose(mask_np, mask_tf_val)
+
+
+def test_anchor_negative_triplet_mask():
+    num_data = 64
+    num_classes = 10
+
+    labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
+
+    mask_np = np.zeros((num_data, num_data))
+    for i in range(num_data):
+        for k in range(num_data):
+            distinct = (i != k)
+            valid = (labels[i] != labels[k])
+            mask_np[i, k] = (distinct and valid)
+
+    mask_tf = _get_anchor_negative_triplet_mask(labels)
+    with tf.Session() as sess:
+        mask_tf_val = sess.run(mask_tf)
+
+    assert np.allclose(mask_np, mask_tf_val)
+
+
+def test_simple_batch_all_triplet_loss():
+    """Test the triplet loss with batch all triplet mining in a simple case where
+    there is just one class."""
+    num_data = 10
+    feat_dim = 6
+    margin = 0.2
+    num_classes = 1
+
+    embeddings = np.random.rand(num_data, feat_dim).astype(np.float32)
+    labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
+
+    for squared in [True, False]:
+        loss_np = 0.0
+
+        # Compute the loss in TF.
+        loss_tf, fraction = batch_all_triplet_loss(labels, embeddings, margin, squared=squared)
         with tf.Session() as sess:
-            for squared in [True, False]:
-                res_np = pairwise_distance_np(embeddings, squared=squared)
-                res_tf = sess.run(_pairwise_distances(embeddings, squared=squared))
-                assert np.allclose(res_np, res_tf)
+            loss_tf_val, fraction_val = sess.run([loss_tf, fraction])
+        assert np.allclose(loss_np, loss_tf_val)
+        assert np.allclose(fraction_val, 0.0)
 
 
-    def test_triplet_mask(self):
-        num_data = 64
-        num_classes = 10
+def test_batch_all_triplet_loss():
+    """Test the triplet loss with batch all triplet mining"""
+    num_data = 10
+    feat_dim = 6
+    margin = 0.2
+    num_classes = 5
 
-        labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
+    embeddings = np.random.rand(num_data, feat_dim).astype(np.float32)
+    labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
 
-        mask_np = np.zeros((num_data, num_data, num_data))
+    for squared in [True, False]:
+        pdist_matrix = pairwise_distance_np(embeddings, squared=squared)
+
+        loss_np = 0.0
+        num_positives = 0.0
+        num_valid = 0.0
         for i in range(num_data):
             for j in range(num_data):
                 for k in range(num_data):
                     distinct = (i != j and i != j and j != k)
                     valid = (labels[i] == labels[j]) and (labels[i] != labels[k])
-                    mask_np[i, j, k] = (distinct and valid)
+                    if distinct and valid:
+                        num_valid += 1.0
 
-        mask_tf = _get_triplet_mask(labels)
+                        pos_distance = pdist_matrix[i][j]
+                        neg_distance = pdist_matrix[i][k]
+
+                        loss = np.maximum(0.0, pos_distance - neg_distance + margin)
+                        loss_np += loss
+
+                        if loss > 0:
+                            num_positives += 1.0
+
+        loss_np /= num_positives
+
+        # Compute the loss in TF.
+        loss_tf, fraction = batch_all_triplet_loss(labels, embeddings, margin, squared=squared)
         with tf.Session() as sess:
-            mask_tf_val = sess.run(mask_tf)
+            loss_tf_val, fraction_val = sess.run([loss_tf, fraction])
+        assert np.allclose(loss_np, loss_tf_val)
+        assert np.allclose(num_positives / num_valid, fraction_val)
 
-        assert np.allclose(mask_np, mask_tf_val)
 
-    def test_anchor_positive_triplet_mask(self):
-        num_data = 64
-        num_classes = 10
+def test_batch_hard_triplet_loss():
+    """Test the triplet loss with batch hard triplet mining"""
+    num_data = 50
+    feat_dim = 6
+    margin = 0.2
+    num_classes = 5
 
-        labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
+    embeddings = np.random.rand(num_data, feat_dim).astype(np.float32)
+    labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
 
-        mask_np = np.zeros((num_data, num_data))
+    for squared in [True, False]:
+        pdist_matrix = pairwise_distance_np(embeddings, squared=squared)
+
+        loss_np = 0.0
+        num_positives = 0.0
+        num_valid = 0.0
         for i in range(num_data):
-            for j in range(num_data):
-                distinct = (i != j)
-                valid = labels[i] == labels[j]
-                mask_np[i, j] = (distinct and valid)
+            # Select the hardest positive
+            max_pos_dist = np.max(pdist_matrix[i][labels==labels[i]])
 
-        mask_tf = _get_anchor_positive_triplet_mask(labels)
+            # Select the hardest negative
+            min_neg_dist = np.min(pdist_matrix[i][labels!=labels[i]])
+
+            loss = np.maximum(0.0, max_pos_dist - min_neg_dist + margin)
+            loss_np += loss
+
+        loss_np /= num_data
+
+        # Compute the loss in TF.
+        loss_tf = batch_hard_triplet_loss(labels, embeddings, margin, squared=squared)
         with tf.Session() as sess:
-            mask_tf_val = sess.run(mask_tf)
-
-        assert np.allclose(mask_np, mask_tf_val)
-
-    def test_anchor_negative_triplet_mask(self):
-        num_data = 64
-        num_classes = 10
-
-        labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
-
-        mask_np = np.zeros((num_data, num_data))
-        for i in range(num_data):
-            for k in range(num_data):
-                distinct = (i != k)
-                valid = (labels[i] != labels[k])
-                mask_np[i, k] = (distinct and valid)
-
-        mask_tf = _get_anchor_negative_triplet_mask(labels)
-        with tf.Session() as sess:
-            mask_tf_val = sess.run(mask_tf)
-
-        assert np.allclose(mask_np, mask_tf_val)
-
-    def test_simple_batch_all_triplet_loss(self):
-        """Test the triplet loss with batch all triplet mining in a simple case where
-        there is just one class."""
-        num_data = 10
-        feat_dim = 6
-        margin = 0.2
-        num_classes = 1
-
-        embeddings = np.random.rand(num_data, feat_dim).astype(np.float32)
-        labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
-
-        for squared in [True, False]:
-            loss_np = 0.0
-
-            # Compute the loss in TF.
-            loss_tf, fraction = batch_all_triplet_loss(labels, embeddings, margin, squared=squared)
-            with tf.Session() as sess:
-                loss_tf_val, fraction_val = sess.run([loss_tf, fraction])
-            assert np.allclose(loss_np, loss_tf_val)
-            assert np.allclose(fraction_val, 0.0)
-
-    def test_batch_all_triplet_loss(self):
-        """Test the triplet loss with batch all triplet mining"""
-        num_data = 10
-        feat_dim = 6
-        margin = 0.2
-        num_classes = 5
-
-        embeddings = np.random.rand(num_data, feat_dim).astype(np.float32)
-        labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
-
-        for squared in [True, False]:
-            pdist_matrix = pairwise_distance_np(embeddings, squared=squared)
-
-            loss_np = 0.0
-            num_positives = 0.0
-            num_valid = 0.0
-            for i in range(num_data):
-                for j in range(num_data):
-                    for k in range(num_data):
-                        distinct = (i != j and i != j and j != k)
-                        valid = (labels[i] == labels[j]) and (labels[i] != labels[k])
-                        if distinct and valid:
-                            num_valid += 1.0
-
-                            pos_distance = pdist_matrix[i][j]
-                            neg_distance = pdist_matrix[i][k]
-
-                            loss = np.maximum(0.0, pos_distance - neg_distance + margin)
-                            loss_np += loss
-
-                            if loss > 0:
-                                num_positives += 1.0
-
-            loss_np /= num_positives
-
-            # Compute the loss in TF.
-            loss_tf, fraction = batch_all_triplet_loss(labels, embeddings, margin, squared=squared)
-            with tf.Session() as sess:
-                loss_tf_val, fraction_val = sess.run([loss_tf, fraction])
-            assert np.allclose(loss_np, loss_tf_val)
-            assert np.allclose(num_positives / num_valid, fraction_val)
-
-    def test_batch_hard_triplet_loss(self):
-        """Test the triplet loss with batch hard triplet mining"""
-        num_data = 50
-        feat_dim = 6
-        margin = 0.2
-        num_classes = 5
-
-        embeddings = np.random.rand(num_data, feat_dim).astype(np.float32)
-        labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
-
-        for squared in [True, False]:
-            pdist_matrix = pairwise_distance_np(embeddings, squared=squared)
-
-            loss_np = 0.0
-            num_positives = 0.0
-            num_valid = 0.0
-            for i in range(num_data):
-                # Select the hardest positive
-                max_pos_dist = np.max(pdist_matrix[i][labels==labels[i]])
-
-                # Select the hardest negative
-                min_neg_dist = np.min(pdist_matrix[i][labels!=labels[i]])
-
-                loss = np.maximum(0.0, max_pos_dist - min_neg_dist + margin)
-                loss_np += loss
-
-            loss_np /= num_data
-
-            # Compute the loss in TF.
-            loss_tf = batch_hard_triplet_loss(labels, embeddings, margin, squared=squared)
-            with tf.Session() as sess:
-                loss_tf_val = sess.run(loss_tf)
-            assert np.allclose(loss_np, loss_tf_val)
-
-
-if __name__ == '__main__':
-    unittest.main()
+            loss_tf_val = sess.run(loss_tf)
+        assert np.allclose(loss_np, loss_tf_val)

model/triplet_loss.py
@@ -3,6 +3,45 @@
 import tensorflow as tf
 
 
+def _pairwise_distances(embeddings, squared=False):
+    """Compute the 2D matrix of distances between all the embeddings.
+
+    Args:
+        embeddings: tensor of shape (batch_size, embed_dim)
+        squared: Boolean. If true, output is the pairwise squared euclidean distance matrix.
+                 If false, output is the pairwise euclidean distance matrix.
+
+    Returns:
+        pairwise_distances: tensor of shape (batch_size, batch_size)
+    """
+    # Get the dot product between all embeddings
+    # shape (batch_size, batch_size)
+    dot_product = tf.matmul(embeddings, tf.transpose(embeddings))
+
+    # Get squared L2 norm for each embedding. We can just take the diagonal of `dot_product`.
+    # This also provides more numerical stability (the diagonal of the result will be exactly 0).
+    # shape (batch_size,)
+    square_norm = tf.diag_part(dot_product)
+
+    # Compute the pairwise distance matrix as we have:
+    # ||a - b||^2 = ||a||^2  - 2 <a, b> + ||b||^2
+    # shape (batch_size, batch_size)
+    distances = tf.expand_dims(square_norm, 0) - 2.0 * dot_product + tf.expand_dims(square_norm, 1)
+
+    if not squared:
+        # Because the gradient of sqrt is infinite when distances == 0.0 (ex: on the diagonal)
+        # we need to add a small epsilon where distances == 0.0
+        mask = tf.to_float(tf.equal(distances, 0.0))
+        distances = distances + mask * 1e-16
+
+        distances = tf.sqrt(distances)
+
+        # Correct the epsilon added: set the distances on the mask to be exactly 0.0
+        distances = distances * (1.0 - mask)
+
+    return distances
+
+
 def _get_anchor_positive_triplet_mask(labels):
     """Return a 2D mask where mask[a, p] is True iff a and p are distinct and have same label.
 
@@ -77,37 +116,6 @@ def _get_triplet_mask(labels):
     return mask
 
 
-def _pairwise_distances(embeddings, squared=False):
-    """Compute the 2D matrix of distances between all the embeddings.
-
-    Args:
-        embeddings: tensor of shape (batch_size, embed_dim)
-        squared: Boolean. If true, output is the pairwise squared euclidean distance matrix.
-                 If false, output is the pairwise euclidean distance matrix.
-
-    Returns:
-        pairwise_distances: tensor of shape (batch_size, batch_size)
-    """
-    # Get the dot product between all embeddings
-    # shape (batch_size, batch_size)
-    dot_product = tf.matmul(embeddings, tf.transpose(embeddings, [1, 0]))
-
-    # Get squared L2 norm for each embedding. We can just take the diagonal of `dot_product`.
-    # This also provides more numerical stability (the diagonal of the result will be exactly 0).
-    # shape (batch_size,)
-    square_norm = tf.diag_part(dot_product)
-
-    # Compute the pairwise distance matrix as we have:
-    # ||a - b||^2 = ||a||^2  - 2 <a, b> + ||b||^2
-    # shape (batch_size, batch_size)
-    distances = tf.expand_dims(square_norm, 0) - 2.0 * dot_product + tf.expand_dims(square_norm, 1)
-
-    if not squared:
-        distances = tf.sqrt(distances)
-
-    return distances
-
-
 def batch_all_triplet_loss(labels, embeddings, margin, squared=False):
     """Build the triplet loss over a batch of embeddings.
 
@@ -142,14 +150,14 @@ def batch_all_triplet_loss(labels, embeddings, margin, squared=False):
     # Put to zero the invalid triplets
     # (where label(a) != label(p) or label(n) == label(a) or a == p)
     mask = _get_triplet_mask(labels)
-    mask = tf.cast(mask, tf.float32)
+    mask = tf.to_float(mask)
     triplet_loss = tf.multiply(mask, triplet_loss)
 
     # Remove negative losses (i.e. the easy triplets)
     triplet_loss = tf.maximum(triplet_loss, 0.0)
 
     # Count number of positive triplets (where triplet_loss > 0)
-    valid_triplets = tf.cast(tf.greater(triplet_loss, 1e-16), tf.float32)
+    valid_triplets = tf.to_float(tf.greater(triplet_loss, 1e-16))
     num_positive_triplets = tf.reduce_sum(valid_triplets)
     num_valid_triplets = tf.reduce_sum(mask)
     fraction_positive_triplets = num_positive_triplets / (num_valid_triplets + 1e-16)
@@ -181,7 +189,7 @@ def batch_hard_triplet_loss(labels, embeddings, margin, squared=False):
     # For each anchor, get the hardest positive
     # First, we need to get a mask for every valid positive (they should have same label)
     mask_anchor_positive = _get_anchor_positive_triplet_mask(labels)
-    mask_anchor_positive = tf.cast(mask_anchor_positive, tf.float32)
+    mask_anchor_positive = tf.to_float(mask_anchor_positive)
 
     # We put to 0 any element where (a, p) is not valid (valid if a != p and label(a) == label(p))
     anchor_positive_dist = tf.multiply(mask_anchor_positive, pairwise_dist)
@@ -192,7 +200,7 @@ def batch_hard_triplet_loss(labels, embeddings, margin, squared=False):
     # For each anchor, get the hardest negative
     # First, we need to get a mask for every valid negative (they should have different labels)
     mask_anchor_negative = _get_anchor_negative_triplet_mask(labels)
-    mask_anchor_negative = tf.cast(mask_anchor_negative, tf.float32)
+    mask_anchor_negative = tf.to_float(mask_anchor_negative)
 
     # We add the maximum value in each row to the invalid negatives (label(a) == label(n))
     max_anchor_negative_dist = tf.reduce_max(pairwise_dist, axis=1, keepdims=True)

train.py
@@ -47,8 +47,7 @@ if __name__ == '__main__':
 
     # Create the input data pipeline
     logging.info("Creating the datasets...")
-    data_dir = args.data_dir
-    data = tf.contrib.learn.datasets.load_dataset("mnist")
+    data = tf.contrib.learn.datasets.mnist.load_mnist(args.data_dir)
 
     # Specify the sizes of the dataset we train on and evaluate on
     params.train_size = data.train.num_examples